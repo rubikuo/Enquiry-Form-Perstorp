0542b55a6ba80435d0fa6125957f08517cbe31be
Rubi Kuo <rubitiara@gmail.com>
2019-05-07 14:48:24 +0200

    ready to test

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// ------------------ Rubi's javascript code ----------------// 
+// ------------------ Rubi's javascript code ----------------//
+$(() => {
+ /*---- to show or hide special requirment ------*/
+ 
+  $("#bulk").click(() => {
+    $("#ifBulk").css("display", "block");
+  });
+
+  $("#packed").click(() => {
+    $("#ifBulk").css("display", "none");
+  });
+
+  $("#both").click(() => {
+    $("#ifBulk").css("display", "block");
+  });
+
+  
+});
