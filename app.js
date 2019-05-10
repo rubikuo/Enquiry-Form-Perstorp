22c5cbfeab2fac9e686b0b37edba59f062401d68
Adrienn Timko <adrienn.timko@gmail.com>
2019-05-10 13:59:17 +0200

    updated JS

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
-// ------------------ Rubi’s javascript code ----------------//
+// ------------------ Rubi's javascript code ----------------//
 $(() => {
-  /*---- to show or hide special requirment ------*/
- 
+  /* Request 11 ---- to show or hide special requirment ------*/
+
   $("#bulk").click(() => {
     $("#ifBulk").css("display", "block");
   });
- 
+
   $("#packed").click(() => {
     $("#ifBulk").css("display", "none");
   });
- 
+
   $("#both").click(() => {
     $("#ifBulk").css("display", "block");
   });
- 
- 
-  /*--- to disable special requirement checkbox when none is clicked ----*/
- 
+
+
+  /*--- request 13 to disable special requirement checkbox when none is clicked ----*/
+
   $("#none").change(function() {
     if (this.checked) {
       $(".special").prop("checked", false);
     }
   });
- 
-  /*--- to disable none checkbox when requestments are clicked ----*/
- 
+
+  /*--- request 13 to disable none checkbox when requestments are clicked ----*/
+
   $(".special").change(function() {
     if (this.checked) {
       $("#none").prop("checked", false);
     }
   });
- 
+
+  /*----- request 15 if current load is clicked, datepicker is required ------------*/
+  
+  $("#calender").prop("required", true); // its to match the default setting
+
+  $("#quoteType1").change(function(){
+     if (this.checked) {
+       $("#calender").prop("required", true);
+     } 
+  });
+
+  $("#quoteType2").change(function(){
+    if (this.checked) {
+      $("#calender").prop("required", false);
+    } 
+ });
+
+
+
+
   /*--- to disable the submit button when the check box is not selected ---*/
   const submitBtn = $("#submit-btn");
   submitBtn.attr("disabled", "disabled");
- 
+
   $('input[type="checkbox"]').click(function() {
-    if ($(this).prop("checked") === true) {
+    if (this.checked) {
       submitBtn.removeAttr("disabled");
     } else {
       submitBtn.attr("disabled", "disabled");
     }
   });
- });
+});
--- app2.js
+++ app2.js
@@ -5,62 +5,14 @@
 //-----request: if temperature checkbox is clicked (id="special2"), then the input ˚C is required (id="temperature"----//
 //-----the input formate must be number between 0 and 200 ------------------//
 
-//----solution1: if (special2 is clicked) then { 0 <= #temperature <= 200 } -------//
-//---------------else if (temperature is not empty && special2 NOT clicked then { message on the screen: ''} or set it to checked----//
-
 $(() => {
     $('#special2').change(function() { 
          if (this.checked) {
              $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
          } else if (('#temperature').value.length !== 0) {
-            alert("message") ;//not working
-            $(this).prop('checked', true); //should check the box, but it doesn't check
+           (this).prop('checked', true); //should check the box, but it doesn't check
          }
     });
 });
 
 
-/* attr (jquery 1.6), prop, function(e) e.target.checked, isNumeric, is(':checked) = it just checks if it is true, val() instead of value, return true, else if (('#temperature').value.length !== 0) 
-NOT CHECKING; IF THERE IS A VALUE:
-
-$('#special2').change(function() { 
-         if (this.checked) {
-             $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
-         } else if (('#temperature').value) {
-             $(this).prop('checked',true); //should check the box, but it doesn't check
-         }
-    });
-
-{
-$('#special2').change(function() { 
-         if (this.checked) {
-             $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
-         } else if (('#temperature').value) {
-            $(this).prop('checked',true);
-         }
-    });
-NOT CHECKING; IF THERE IS A VALUE:
-{
-    $('#special2').change(function(e) { 
-         if (e.target.checked) {
-             $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
-         } else if (('#temperature').value) {
-            $(e.target).prop('checked',true);
-         }
-    });
-   
-it doesn't work either with attr
- EMPTY STRING ALWAYS FALSY!!!
-   else if (('#temperature') !== '' && $('#special2').prop('checked',false) {
-             $('.error-message').text('Requested unloading temperature is not selected! Please select it.').fadeIn();
-         }
-
-   if (('#special2').click()){
-            0 <= ('#temperature') <= 200;
-        } else if ('#temperature' !== '' && ('#special2').not(click()) ) {
-            alert('Requested unloading temperature is not selected! Please select it.');
-        }*/
-        
-//----or solution2: if special2 NOT clicked { can't fill in temperature } ------//
-//--------------esle if special2 is clicked { temperature must be -----------//
-//-----------------------------a number between 0 and 200 } ----&& ()-----('#special2').not(click())?-----//
