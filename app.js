d825fcdf91eabfb39c7a29f4a054f9ddc0bbad4a
Adrienn Timko <adrienn.timko@gmail.com>
2019-05-10 13:50:28 +0200

    request12 ready test, it is reguest with G!

--- app.js
+++ app.js
@@ -1,68 +1,45 @@
-// ------------------ Rubi's javascript code ----------------//
+// ------------------ Rubi’s javascript code ----------------//
 $(() => {
   /*---- to show or hide special requirment ------*/
-
+ 
   $("#bulk").click(() => {
     $("#ifBulk").css("display", "block");
   });
-
+ 
   $("#packed").click(() => {
     $("#ifBulk").css("display", "none");
   });
-
+ 
   $("#both").click(() => {
     $("#ifBulk").css("display", "block");
   });
-
-  /*---- to disable special requestments checkbox when none is selected ----*/
-  // const specialrequirements = $(".special");
-  // $("#none").click(function() {
-  //   if ($(this).prop("checked") === true) {
-  //     specialrequirements.removeAttr("checked");
-  //     specialrequirements.attr("disabled", "disabled");
-  //   } else {
-  //     specialrequirements.removeAttr("disabled");
-  //   }
-  // });
-
-  /*--- to disable none checkbox when requestments are clicked ----*/
-  // var $others = $('input[name="meh"]').not('#omgwtfbbq')
-  // $('#omgwtfbbq').change(function () {
-  //     if (this.checked) {
-  //         $others.prop('checked', false)
-  //     }
-  // });
-  // $others.change(function () {
-  //     if (this.checked) {
-  //         $('#omgwtfbbq').prop('checked', false)
-  //     }
-  // })
-
-
-   
-  $("#none").change(function () {
-        if (this.checked) {
-          $(".special").prop("checked",false);
-        }
+ 
+ 
+  /*--- to disable special requirement checkbox when none is clicked ----*/
+ 
+  $("#none").change(function() {
+    if (this.checked) {
+      $(".special").prop("checked", false);
+    }
   });
-   $(".special").change(function () {
-     if (this.checked) {
-       $("#none").prop("checked",false);
-     }
-   });
-
-});
-
-/*--- to disable the submit button when the check box is not selected ---*/
-
-const submitBtn = $("#submit-btn");
-submitBtn.attr("disabled", "disabled");
-
-$('input[type="checkbox"]').click(function() {
-  if ($(this).prop("checked") == true) {
-    submitBtn.removeAttr("disabled");
-  } else {
-    submitBtn.attr("disabled", "disabled");
-  }
-});
-ve
+ 
+  /*--- to disable none checkbox when requestments are clicked ----*/
+ 
+  $(".special").change(function() {
+    if (this.checked) {
+      $("#none").prop("checked", false);
+    }
+  });
+ 
+  /*--- to disable the submit button when the check box is not selected ---*/
+  const submitBtn = $("#submit-btn");
+  submitBtn.attr("disabled", "disabled");
+ 
+  $('input[type="checkbox"]').click(function() {
+    if ($(this).prop("checked") === true) {
+      submitBtn.removeAttr("disabled");
+    } else {
+      submitBtn.attr("disabled", "disabled");
+    }
+  });
+ });
