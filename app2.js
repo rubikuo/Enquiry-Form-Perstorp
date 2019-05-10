1dc42f27b875f183473379d3a73f497fe211d784
Rubi Kuo <rubitiara@gmail.com>
2019-05-10 15:05:26 +0200

    Rubi test

--- app2.js
+++ app2.js
@@ -6,13 +6,18 @@
 //-----the input formate must be number between 0 and 200 ------------------//
 
 $(() => {
-    $('#special2').change(function() {
-         if (this.checked) {
-             $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
-         } else if (('#temperature').value) {
-           (this).prop('checked', true); //should check the box, but it doesn't check
-         }
-    });
- });
+  $("#special2").click(function() {
+    if (this.checked) {
+      $("#temperature").prop("required", true); //add required to #temperature, if special2 checkbox is checked
+    }
+  });
 
+  $("#special2").prop("required", false);
 
+  // if the input value is filled out, the checkbox is required. 
+  // problem is if delete input value, required still there ( need to solve) 
+  $("#temperature").change(function() {
+    $("#special2").prop("required", true);
+  });
+
+});
