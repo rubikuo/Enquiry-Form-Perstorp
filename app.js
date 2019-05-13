bb11d5937b4d3ebc7a8427fec58b3688ee54e873
Rubi Kuo <rubitiara@gmail.com>
2019-05-13 12:45:53 +0200

    temperature input & special2 checkbox worked

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ $(() => {
     $("#ifBulk").css("display", "block");
   });
 
+  /* ----- request 12 if temperatue is required ---*/
+
+  $("#temperature").val(""); 
+
+  $("#special2").change(function() {
+    if (this.checked) {
+      $("#temperature").prop("required", true);
+    } else {
+      $("#temperature").prop("required", false);
+      $("#temperature").val("");
+    }
+  });
+
+  $("#temperature").change(function() {
+    if ($(this).val().length > 0) {
+      $("#special2").prop("checked", true);
+      $(this).prop("required", true);
+    }
+  });
 
   /*--- request 13 to disable special requirement checkbox when none is clicked ----*/
 
@@ -32,35 +51,35 @@ $(() => {
   });
 
   /*----- request 15 if current load is clicked, datepicker is required ------------*/
-  
+
   $("#calender").prop("required", true); // its to match the default setting
 
-  $("#quoteType1").change(function(){
-     if (this.checked) {
-       $("#calender").prop("required", true);
-     } 
-  });
-
-  $("#quoteType2").change(function(){
+  $("#quoteType1").change(function() {
     if (this.checked) {
-      $("#calender").prop("required", false);
-    } 
- });
-
-
-
-
-  /*--- to disable the submit button when the check box is not selected ---*/
-  const submitBtn = $("#submit-btn");
-  submitBtn.attr("disabled", "disabled");
-
-  $('input[type="checkbox"]').click(function() {
-    if (this.checked) {
-      submitBtn.removeAttr("disabled");
-    } else {
-      submitBtn.attr("disabled", "disabled");
+      $("#calender").prop("required", true);
     }
   });
+
+  $("#quoteType2").change(function() {
+    if (this.checked) {
+      $("#calender").prop("required", false);
+    }
+  });
+
+  /*--- to disable the submit button when the check box is not selected ---*/
+  // const submitBtn = $("#submit-btn");
+  // submitBtn.prop("disabled", true);
+
+  // $(".specail").change(function() {
+  //   if (this.checked) {
+  //     submitBtn.prop("disabled", false);
+  //   } else {
+  //     submitBtn.prop("disabled", true);
+  //   }
+  //   $("#packed").change(function () {
+  //     if (this.checked) {
+  //       submitBtn.prop("disabled", false);
+  //     }
+
+  // });
 });
-
-
--- app2.js
+++ app2.js
@@ -6,13 +6,11 @@
 //-----the input formate must be number between 0 and 200 ------------------//
 
 $(() => {
-    $('#special2').change(function() {
-         if (this.checked) {
-             $('#temperature').prop('required',true); //add required to #temperature, if special2 checkbox is checked
-         } else if (('#temperature').value) {
-           (this).prop('checked', true); //should check the box, but it doesn't check
-         }
-    });
+  
+     
+
+
+      
  });
 
 
